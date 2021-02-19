50377868530a7fa4fed11aaebe3cbb06adbfbab8
m <g>
2021-02-19 18:48:27 +0100

    TriangleValidator refactor

--- src/main/java/m/gaw/kafkademo/implementation/components/TriangleValidator.java
+++ src/main/java/m/gaw/kafkademo/implementation/components/TriangleValidator.java
@@ -4,25 +4,30 @@ import m.gaw.kafkademo.abstraction.components.Validator;
 import m.gaw.kafkademo.implementation.model.Triangle;
 import org.springframework.stereotype.Component;
 
-import static java.lang.Math.max;
+import java.util.Arrays;
 
 @Component
-public class TriangleValidator implements Validator<Triangle> {
+public class  TriangleValidator implements Validator<Triangle> {
 
     public boolean isValid(Triangle triangle) {
-        return hasPositiveSides(triangle) && canBeConstructed(triangle);
-    }
-
-    private boolean canBeConstructed(Triangle triangle) {
-        int longest = max(triangle.getA(), max(triangle.getB(), triangle.getC()));
-        int circuit = triangle.getA() + triangle.getB() + triangle.getC();
-        return 2 * longest < circuit;
+        return hasPositiveSides(triangle)
+                && canBeConstructed(triangle);
     }
 
     private boolean hasPositiveSides(Triangle triangle) {
         return triangle.getA() > 0
                 && triangle.getB() > 0
                 && triangle.getC() > 0;
+    }
+
+    private boolean canBeConstructed(Triangle triangle) {
+        int[] sides = {triangle.getA(), triangle.getB(), triangle.getC()};
+
+        int circuit = Arrays.stream(sides)
+                .sum();
+
+        return Arrays.stream(sides)
+                .noneMatch(side -> 2*side >= circuit);
     }
 
 }
--- src/main/java/m/gaw/kafkademo/implementation/model/Triangle.java
+++ src/main/java/m/gaw/kafkademo/implementation/model/Triangle.java
@@ -1,21 +1,11 @@
 package m.gaw.kafkademo.implementation.model;
 
+import lombok.Getter;
 import m.gaw.kafkademo.abstraction.model.ValidatedObject;
 
+@Getter
 public class Triangle extends ValidatedObject {
     private int a;
     private int b;
     private int c;
-
-    public int getA() {
-        return a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public int getC() {
-        return c;
-    }
 }
